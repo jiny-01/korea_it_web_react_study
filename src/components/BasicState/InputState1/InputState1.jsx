2e89ba36804ea45ffab2953c0fa54b3c82736ae5
jiny01 <allie7019@naver.com>
2025-08-11 19:12:15 +0900

    feat: git 자격증명

---
+++ src/components/BasicState/InputState1/InputState1.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function InputState1() {
+  return (
+    <div>
+        <h1></h1>
+        <h1></h1>
+        <input type="text" />
+        <input type="text" />
+        <button>확인</button>
+    </div>
+  )
+}
+
+export default InputState1
