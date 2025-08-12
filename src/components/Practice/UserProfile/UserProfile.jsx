11d40508419b704ff105f8c574a37fa3419e0b6e
jiny01 <allie7019@naver.com>
2025-08-12 21:36:00 +0900

    feat: UserProfile

--- src/components/Practice/UserProfile/UserProfile.jsx
+++ src/components/Practice/UserProfile/UserProfile.jsx
@@ -11,37 +11,55 @@ function UserProfile() {
 
   const [error, setError] = useState(null);
 
-
   const fetchUser = async () => {
-      try {
-        const response = await fetch(API_URL);
-        if (!response.ok) {
-          throw new Error("데이터를 불러오는 데 실패했습니다.");
-        }
-
-        //response.ok 가 ok 면 가져와서 set
-        const responseData = await response.json();
-        setUser(responseData);
-      } catch (error) {
-        setError(error.message);
-      } finally {
-        setIsLoading(false);     //초기값이 true 이므로 로딩 풀어줘야함
+    try {
+      const response = await fetch(API_URL);
+      if (!response.ok) {
+        throw new Error("데이터를 불러오는 데 실패했습니다.");
       }
-    };
+
+      //response.ok 가 ok 면 가져와서 set
+      const responseData = await response.json();
+      setUser(responseData);
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setIsLoading(false); //초기값이 true 이므로 로딩 풀어줘야함
+    }
+  };
 
   //유저 프로필 최초로 뜰 떄 요청 보내서 setUser
   useEffect(() => {
     fetchUser();
-}, []);
+  }, []);
+
+  if (isLoading) {
+    return <div>불러오는 중...</div>;
+  }
+
+  if (error) {
+    return <div>에러: {error}</div>;
+  }
+
   return (
     <div>
       <div>
-        <p>이름: {}</p>
-        <p>이메일: {}</p>
-        <p>전화번호: {}</p>
+        {user ? (
+          <div>
+            <p>이름: {user.name}</p>
+            <p>이메일: {user.email}</p>
+            <p>전화번호: {user.phone}</p>
+          </div>
+        ) : (
+          <p>데이터가 없습니다.</p>
+        )}
       </div>
     </div>
   );
 }
 
+//fetchUser 로직에 finally로 isloading 을 false 로 바꾸기 때문에 
+//불러오는 중... -> 있으면 user 정보, 없으면 데이터 없음
+
+//만약 에러 -> 유저 있는지 확인 -> 있으면 user 정보, 없으면 데이터 없음
 export default UserProfile;
